ff195f400d0626e0cfbeb05234b61f30d78e968e
nageshbl <nageshbl@ae9f08c5-8244-0410-8902-b5678db2bf8b>
2008-02-10 17:51:38 +0000

    added implementation of parseArgs()

--- trunk/code/src/cs6310/proj1/data/Option.java
+++ trunk/code/src/cs6310/proj1/data/Option.java
@@ -11,24 +11,157 @@ package cs6310.proj1.data;
  * 
  */
 public class Option {
-	private int Dimension;
-	private int maxIterations;
+	private int dimension;
+	private int maxIterations = 100;
 	private EdgeTemperature edgeTemperature;
-	private float stopPrecison;
+	private float stopPrecison = 0.001f;
+	
+	public Option() {
+		edgeTemperature = new EdgeTemperature();
+	}
 	
 	
 	public boolean parseArgs(String[] args) {
-		return false;
+		
+		boolean success = true;
+		String [] mandArgs = {"-d", "-l", "-r", "-t", "-b"};
+		String [] optArgs = {"-n", "-p"};
+		
+		int [] mandCount = new int[mandArgs.length];
+		int [] optCount = new int[optArgs.length];
+
+		//Object [] mandValue = new Object[mandArgs.length];
+		//Object [] optValue = new Object[optArgs.length];
+
+		try {
+
+			String arg;
+			String argValue;
+			for (int i = 0; i < args.length - 1; i += 2) {
+				arg = args[i];
+				argValue = args[i + 1];
+				
+				int j;
+				boolean found = false;
+				for (j = 0; j < mandArgs.length; j++) {					
+					if (arg.equals(mandArgs[j])) {						
+						found = true;
+						break;
+					}				
+				}
+
+				if (true == found) {
+						if (0 == mandCount[j]) {
+						int value = Integer.valueOf(argValue).intValue();
+						switch(j) {
+						case 0:
+							if (0 < value) {
+								dimension = value;	
+							}
+							else {
+								throw new Exception();
+							}							 
+							break;
+						case 1:
+							if (0 <= value && 100 >= value) {
+								edgeTemperature.setLeft((float)value);	
+							}							
+							break;
+						case 2:
+							if (0 <= value && 100 >= value) {
+								edgeTemperature.setRight((float)value);								
+							}
+							else {
+								throw new Exception();								
+							}
+
+							break;
+						case 3:
+							if (0 <= value && 100 >= value) {
+								edgeTemperature.setTop((float)value);								
+							}
+							else {
+								throw new Exception();								
+							}
+							break;
+						case 4:
+							if (0 <= value && 100 >= value) {
+								edgeTemperature.setBottom((float)value);								
+							}
+							else {
+								throw new Exception();								
+							}
+							break;
+						}
+						mandCount[j] = 1;
+					}
+					else {
+						throw new Exception();
+					}
+					continue;
+				}
+				
+				for (j = 0; j < optArgs.length; j++) {
+					if (arg.equals(optArgs[j])) {
+						found = true;
+						break;
+					}				
+				}
+
+				if (true == found) {
+					if (0 == optCount[j]) {
+						switch(j) {
+						case 0:
+							int intValue = Integer.valueOf(argValue).intValue();
+							if ( 0 <= intValue) {
+								maxIterations = intValue; 								
+							}
+							else {
+								throw new Exception();								
+							}
+							break;
+						case 1:
+							float floatValue = Float.valueOf(argValue).floatValue();
+							if (0 < floatValue) {
+								stopPrecison = floatValue;								
+							}
+							else {
+								throw new Exception();
+							}
+
+							break;
+						}						
+						optCount[j] = 1;
+					}
+					else {
+						throw new Exception();
+					}
+					continue;
+				}
+			}
+
+			for (int i = 0; i < mandCount.length; i++) {
+				if (0 == mandCount[i]) {
+					throw new Exception();
+				}
+			}
+
+		}
+		catch(Exception e) {
+			success = false;
+		}
+		
+		return success;
 	}
 
 
 	public int getDimension() {
-    	return Dimension;
+    	return dimension;
     }
 
 
 	public void setDimension(int dimension) {
-    	Dimension = dimension;
+    	this.dimension = dimension;
     }
 
 
@@ -60,6 +193,4 @@ public class Option {
 	public void setStopPrecison(float stopPrecison) {
     	this.stopPrecison = stopPrecison;
     }
-	
-
 }
