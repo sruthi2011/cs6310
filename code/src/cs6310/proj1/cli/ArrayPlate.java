ee761ab3ad966129c19707eaff33deb0889b4caa
nageshbl <nageshbl@ae9f08c5-8244-0410-8902-b5678db2bf8b>
2008-02-10 17:57:52 +0000

    added classes ArrayPlate, ObjectPlate, doPlate, FaPlate

---
+++ code/src/cs6310/proj1/cli/ArrayPlate.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cs6310.proj1.cli;
+
+import cs6310.proj1.data.Plate;
+
+/**
+ * @author Nagesh
+ *
+ */
+public abstract class ArrayPlate extends Plate {
+
+	public ArrayPlate() {
+		super();
+	}
+}
---
+++ code/src/cs6310/proj1/cli/FaPlate.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package cs6310.proj1.cli;
+
+/**
+ * @author Nagesh
+ *
+ */
+public class FaPlate extends ArrayPlate {
+
+	/* (non-Javadoc)
+	 * @see cs6310.proj1.data.Plate#compute()
+	 */
+	public boolean compute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see cs6310.proj1.data.Plate#display()
+	 */
+	public void display() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see cs6310.proj1.data.Plate#stop()
+	 */
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
