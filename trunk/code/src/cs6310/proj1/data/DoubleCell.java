1382b45c8e74a2c458514b575ce2a623e5ae0b82
nageshbl <nageshbl@ae9f08c5-8244-0410-8902-b5678db2bf8b>
2008-02-10 17:53:26 +0000

    added Cell and DoubleCell classes

---
+++ trunk/code/src/cs6310/proj1/data/DoubleCell.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package cs6310.proj1.data;
+
+import java.util.Iterator;
+
+/**
+ * @author Nagesh
+ *
+ */
+public class DoubleCell extends Cell {
+
+	private double temperature;
+	/**
+	 * 
+	 */
+	public DoubleCell() {
+		super();
+		temperature = 0.0;
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the temperature
+	 */
+	public double getTemperature() {
+		return temperature;
+	}
+
+	/**
+	 * @param temperature the temperature to set
+	 */
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+
+	/* (non-Javadoc)
+	 * @see cs6310.proj1.data.Cell#calculateTemperature()
+	 */
+	public void calculateTemperature() {
+		double neighborSum = 0.0;
+		Iterator iterator = neighbors.iterator();
+		
+		while (iterator.hasNext()) {
+			neighborSum += ((DoubleCell)iterator.next()).getTemperature(); 
+		}
+		temperature = neighborSum / 4.0;
+		// TODO Auto-generated method stub
+	}
+}
