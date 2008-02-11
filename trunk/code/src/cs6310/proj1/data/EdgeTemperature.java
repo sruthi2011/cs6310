e67993a834926d45c17b5c4539ef61a07a2a8673
alex.guan <alex.guan@ae9f08c5-8244-0410-8902-b5678db2bf8b>
2008-02-11 05:59:02 +0000

    1) Add init method which will check the parameters 2) Set the default values.

--- trunk/code/src/cs6310/proj1/data/EdgeTemperature.java
+++ trunk/code/src/cs6310/proj1/data/EdgeTemperature.java
@@ -9,6 +9,7 @@ package cs6310.proj1.data;
  *
  */
 public class EdgeTemperature {
+	public static final float DEFAULT_TEMP = (float) 50.0;
 	private float left;
 	private float top;
 	private float right;
@@ -18,16 +19,13 @@ public class EdgeTemperature {
 	 * @param edge temperature values to be set
 	 */
 	EdgeTemperature(float left, float top, float right, float bottom) {
-		this.left = left;
-		this.top = top;
-		this.right = right;
-		this.bottom = bottom;
+		init(left, top, right, bottom);
 	}
 	/**
 	 * @see default constructor
 	 */
 	public EdgeTemperature() {
-		// TODO Auto-generated constructor stub
+		init(DEFAULT_TEMP, DEFAULT_TEMP, DEFAULT_TEMP, DEFAULT_TEMP);
 	}
 	/**
      * @return the left
@@ -76,5 +74,17 @@ public class EdgeTemperature {
      */
     public void setBottom(float bottom) {
     	this.bottom = bottom;
+    }
+    
+    private void init(float left, float top, float right, float bottom) {
+    	assert left >= 0.0 && left <= 100.0;
+    	assert top >= 0.0 && top <= 100.0;
+    	assert right >= 0.0 && right <= 100.0;
+    	assert bottom >= 0.0 && bottom <= 100.0;
+    	
+		this.left = left;
+		this.top = top;
+		this.right = right;
+		this.bottom = bottom;
     }
 }
